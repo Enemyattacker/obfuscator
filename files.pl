4c183d354f1d4baa9e00bdd655cba0e54d8aa9bb
Enemyattacker <tidehunter.yf@gmail.com>
2016-09-25 13:41:26 +0300

    Работа с файлами (чтение кода из входного файла, поиск идентификаторов в коде, и запись найденного в выходной файл)

---
+++ files.pl
@@ -0,0 +1,15 @@
+
+`chcp 65001`;
+
+open input, "input.html" or die "Ошибка открытия файла index.html:$!";
+open output, ">output.html" or die "Ошибка открытия файла output.html:$!";
+
+@_ = <input>;
+close input or die $!;
+
+$_=join"", @_;
+
+@array =  /#[a-z_]+[a-z0-9_]*/gi;
+print output "@array ";
+
+close output or die $!;

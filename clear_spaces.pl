fab25250ff7995580160556cb1009c906e64b660
Enemyattacker <tidehunter.yf@gmail.com>
2016-09-25 13:42:45 +0300

    Сжатие кода за счёт удаления лишних пробелов и символов перехода на новую строку

---
+++ clear_spaces.pl
@@ -0,0 +1,9 @@
+﻿`chcp 65001`;
+$text = "BufferedReader     bufferedReader = new BufferedReader(new FileReader('text.txt'));
+        BufferedWriter           bufferedWriter = new BufferedWriter(new FileWriter('tickets.txt'));
+
+        String string =     bufferedReader.readLine();
+        String[]      strings = string.split(' ');
+        System.out.println('Исходные билетики ' + '\n'    + Arrays.toString(strings));";
+$text =~ tr/" "\n/ /s;
+print "Код без лишних пробелов и переходов на новую строку: \n"."$text \n";
